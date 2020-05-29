58b861294a5fcccf92b5d21ae5f61a22a986bc2d
“admin” <542035984@qq.com>
2020-05-29 17:52:33 +0800

    在文本框内显示信息---

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -3,6 +3,27 @@
 <el-input v-model="ruleForm.username" id="username" placeholder="输入username"></el-input>
    <el-button type="danger" @click="Sousuo()"> 搜索 </el-button>
            <el-button type="danger" @click="getinfo()"> 查询 </el-button>
+
+
+     <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+
     </div>
 </template>
 <script>
@@ -13,18 +34,37 @@
 //, { refs, root }
     export default {
         name: "Login",
+              data() {
+                 return {
+                     tableData: [{
+                         date: '2016-05-02',
+                         name: '王小虎',
+                         address: '上海市普陀区金沙江路 1518 弄'
+                     }, {
+                         date: '2016-05-04',
+                         name: '王小虎',
+                         address: '上海市普陀区金沙江路 1517 弄'
+                     }, {
+                         date: '2016-05-01',
+                         name: '王小虎',
+                         address: '上海市普陀区金沙江路 1519 弄'
+                     }, {
+                         date: '2016-05-03',
+                         name: '王小虎',
+                         address: '上海市普陀区金沙江路 1516 弄'
+                     }]
+                 }
+             },
+
     setup(props,{root}) {
 
         const ruleForm=reactive({
             username:''
         })
         //点击后返回值,使用函数表达式写
-
         const getinfo=(()=>{
             Getinfo()
         })
-
-
         const Sousuo=(()=>{
             if(ruleForm.username == '')
                 root.$message.error('输入内容不能为空')
@@ -39,8 +79,6 @@
                   console.log(error);
                 })
         })
-
-
        //点击提交
         onMounted(()=>{
             // Getinfo()
@@ -49,9 +87,6 @@
             getinfo,
             ruleForm,
             Sousuo
-
-
-
     }}}
 
 </script>
