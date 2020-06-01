f1113cad5c6e8d0cdd17abbb3aa844cbafeb3ae5
“admin” <542035984@qq.com>
2020-06-01 16:31:57 +0800

    添加add ok ---在login首页展示内容---02

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -5,24 +5,24 @@
         <el-button type="danger" @click="getinfo()"> 查询 </el-button>
 
      <el-table
-      :data="tableData"
+      :data="tableData.item"
       style="width: 100%">
       <el-table-column
-        prop="date"
+        prop="id"
         label="日期"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="stu_name"
         label="姓名"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="address"
+        prop="stu_cls_id"
         label="地址">
       </el-table-column>
         <el-table-column
-        prop="sex"
+        prop="stu_sex"
         label="性别">
       </el-table-column>
     </el-table>
@@ -46,36 +46,43 @@
         })
 
 
-        const tableData= reactive([{
-                         date: '2016-05-02',
-                         name: '王小虎',
-                         address: '上海市普陀区金沙江路 1518 弄',
-                         sex: '男'
+        const tableData= reactive({
+                     item:[
+                         {  id: '1',
+                        stu_name : '王小虎',
+                         stu_sex: '上海市普陀区金沙江路 1518 弄',
+                         stu_cls_id: '男'
                      }, {
-                         date: '2016-05-04',
-                         name: '王小虎',
-                         address: '上海市普陀区金沙江路 1517 弄',
-                         sex: '男'
+                         id: '2',
+                         stu_name: '王小虎',
+                         stu_sex: '上海市普陀区金沙江路 1517 弄',
+                         stu_cls_id: '男'
 
                      }, {
-                         date: '2016-05-01',
-                         name: '王小虎',
-                         address: '上海市普陀区金沙江路 1519 弄',
-                        sex: '男'
+                         id: '3',
+                         stu_name: '王小虎',
+                         stu_sex: '上海市普陀区金沙江路 1519 弄',
+                        stu_cls_id: '男'
 
-                     }, {
-                         date: '2016-05-03',
-                         name: '王小虎',
-                         address: '上海市普陀区金沙江路 1516 弄',
-                         sex: '女'
-
-                     }])
+                     }
+                     ]})
 
 
         //点击后返回值,使用函数表达式写
         const getinfo=(()=>{
-            Getinfo()
+            Getinfo({}).then(response=>{
+                let data = response.data
+                console.log("前端===",data)
+                tableData.item = data
+                alert(tableData.item)
+
+            }).catch(error =>{
+
+            })
         })
+
+
+
         const Sousuo=(()=>{
             if(ruleForm.username == '')
                 root.$message.error('输入内容不能为空')
