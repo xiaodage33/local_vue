402a3b60e4b580f7c30d965edf4a5fc582808370
54203 <542035984@qq.com>
2020-06-17 09:17:26 +0800

    优化地址

--- vue.config.js
+++ vue.config.js
@@ -27,8 +27,9 @@ module.exports = {
       hotOnly: false,
       proxy: {
           '/__api__': {
-              target: "http://django-vue", //API服务器的地址  http://www.web-jshtml.cn/api
               // target: "http://192.168.9.240:8989", //API服务器的地址  http://www.web-jshtml.cn/api
+
+              target: "http://192.168.9.240:8989", //API服务器的地址  http://www.web-jshtml.cn/api
 
               // target: "http://www.web-jshtml.cn/productapi/token", //API服务器的地址  http://www.web-jshtml.cn/api
               changeOrigin: true,
