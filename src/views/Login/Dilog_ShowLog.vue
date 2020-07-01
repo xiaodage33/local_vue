5d2a6c21291ada1b977816a01a480424287c81bb
54203 <542035984@qq.com>
2020-07-01 15:45:34 +0800

    锚点跳转 exceptionList.value.length === 0 length丢失问题。。。。从data.   改为ref数据错误一样

--- src/views/Login/Dilog_ShowLog.vue
+++ src/views/Login/Dilog_ShowLog.vue
@@ -1,9 +1,8 @@
 <template>
 <div>
     <!--<el-button type="text" @click="dialogVisible">点击打开 Dialog</el-button>-->
-    <el-dialog class="el-dialog_title"
+    <el-dialog class="el-dialog__headerbtn" btn-vvue
              :fullscreen="quanping"
-            :title="'Pod名字：'+data.pod_name"
             :visible.sync="data.dialog_info_flag"
             width="60%"
             top="5vh"
@@ -11,11 +10,22 @@
             :pod="data.pod_log_info"
             :pod_name="data.pod_name"
             :before-close="handleDialogClose"  >
-        <el-button class="pull-left" type="primary" @click="scrollTo(-1)" style="font-size: 5px" size="mini">上一个异常</el-button>
-        <el-button class="pull-left" type="primary" @click="scrollTo(1)" style="font-size: 5px" size="mini">下一个异常</el-button>
+        <el-button class="pull-left" type="primary" @click="scrollTo(-1)" style="font-size: 5px" size="mini" :disabled="up_scrollTo" >上一个异常</el-button>
+        <el-button class="pull-left" type="primary" @click="scrollTo(1)" style="font-size: 5px" size="mini" :disabled="up_scrollTo_down">下一个异常</el-button>
+
+        <el-select style="padding-left: 10px;" class="pull-left" v-model=" data.sel_id  "    @change="handleBlur" clearable placeholder="请选择第几个错误位置" size="mini">
+            <el-option
+                    v-for="(item,index) in data.options" :key="index"
+                    :value="index"  >
+            </el-option>
+        </el-select>
+
+
         <el-button class="pull-right" type="primary" @click="All_Quanping" style="font-size: 5px" size="mini">全屏</el-button>
         <el-button class="pull-right" type="primary" @click="Quanping_Rest" style="font-size: 5px;"  size="mini">还原</el-button>
-        <el-button class="pull-right" type="primary" :title="infoPod" @click="Cat_Trace(data.pod_name)" style="font-size: 5px;"  size="mini">查看链路</el-button>
+        <el-button class="pull-right"  type="primary" @click="log_flush" v-loading="loading" style="font-size: 5px;"  size="mini">刷新</el-button>
+
+        <el-button class="pull-right" type="primary"  @click="Cat_Trace(data.pod_name)" style="font-size: 5px;"  size="mini" v-show="false">查看链路</el-button>
 <!--//输入查找错误关键字-->
         <div ref="messageBox" class="pod-content-box">
             <div class="pod-content-item" v-for="(item,index) of data.pod_log_arr" :key="index">
@@ -24,14 +34,13 @@
                 <div v-else class="pod-item-text">{{item}}</div>
             </div>
         </div>
-        <span slot="footer" class="dialog-footer">
-            <el-button type="primary" @click="close">关闭</el-button>
-            <el-button type="primary" @click="log_flush" v-loading="loading">刷新</el-button>
-        </span>
+        <!--<span slot="footer" class="dialog-footer">-->
+            <!--<el-button type="primary" @click="close">关闭</el-button>-->
+            <!--<el-button type="primary" @click="log_flush" v-loading="loading">刷新</el-button>-->
+        <!--</span>-->
     </el-dialog>
-
         <!--//显示错误链路-->
-     <Dilog_ShowTrace :flag.sync="dialog_show_detail"  :pod="infoPod"  />
+     <!--<Dilog_ShowTrace :flag.sync="dialog_show_detail"  :pod="infoPod"  />-->
 </div>
 </template>
 <script>
@@ -51,33 +60,44 @@
             type: String,
             default: "" }
         },
-    setup(props,{emit,root,refs}) {
+      setup(props,{emit,root,refs}) {
         const data = reactive({
             dialog_info_flag: false,   //弹窗标记
             pod_log_info:"",
             pod_log_arr: [],
             pod_name:"",
             podname:"",
-            yangshi_podname:""
+            yangshi_podname:"",
+            exceptionList:[],    ////定义总的筛选数
+            currentException:"",   //跳转当前的位置
+            options:[],
+            sel_id:'',
         })
+
         const dialogVisible = ref(false)
         const loading = ref(false)
-        const quanping = ref(false)
+        const quanping = ref(true)    //全屏默认true
         const dialog_show_detail = ref(false)  //弹框传值
-        const infoPod = ref("")   //错误的pod名字
+        // const infoPod = ref("")   //错误的pod名字
         const guanjianzi = ref("")   //关键字数据
-        const exceptionList = ref([])
-        const currentException = ref('')
 
-        watch(() => {data.dialog_info_flag = props.flag   });
+        const exceptionList =ref([])
+        const currentException =ref("")
+
+
+        const up_scrollTo = ref(false)   //是否可以点击false是可以点击
+        const up_scrollTo_down = ref(false)
+
+        watch(() => {data.dialog_info_flag = props.flag });
         const close = () => {
             data.dialog_info_flag=false;
             emit("update:flag", false);   //emit更新prop flag
             data.pod_log_arr ="";
             data.pod_name = " ";
         }
-        const openDialog = () => {
-            getLog()   //查日志
+        const openDialog = () => {  //弹开后立即执行查日志
+            getLog()
+            handleBlur(0)
         }
         const getLog=()=>{
             let requestData = props.pod
@@ -86,7 +106,6 @@
                 data.pod_log_info = response.data.data
                 // console.log('resp',response.data.data)
                 // console.log(str.indexOf(guanjianzi.value) !=-1)
-
                 if(data.pod_log_info){
                     data.pod_log_arr = data.pod_log_info.split('\n');
                 loading.value=false  //。。。
@@ -99,40 +118,72 @@
         }
         const log_flush=()=>{     //点击刷新
             loading.value=true
-            // loading_jiazai.value=true
             getLog()
+            scrollTo()
         }
         const All_Quanping=()=>{
             quanping.value= true  //全屏
-
         }
         const Quanping_Rest=()=>{
             quanping.value= false   //还原全屏
         }
         const Cat_Trace=(pod)=>{  //点击后弹开traceid页面
-            infoPod.value=pod;
+            // infoPod.value=pod;
             dialog_show_detail.value=true;   //弹出dialog
             }
         const scrollTo=(value)=>{
-            if(exceptionList.value.length === 0) {
+            if(exceptionList.value.length === 0) {  //第一次数组就是空 0
+                // console.log("data.exceptionList===",data.exceptionList)
                 exceptionList.value = refs.exception
+                // console.log("给赋值exceptionList.value",data.exceptionList)  //利用refs 给赋值别名为 exception这个数组
             }
-            if (exceptionList.value.length === 0) {
+            if (exceptionList.value.length === 0) {    //判断赋值后数组是否为空
+                alert("没有错误关关键字")
                 return
             }
-            let resIndex = !currentException.value && currentException.value !== 0 ? 0 : currentException.value + value;
-            const resEle = exceptionList.value[resIndex]
+            let resIndex = !currentException.value && currentException.value !== 0 ? 0 : currentException.value + value;  //三目运算
+               //下面方法改三目运算 resIndex的值是多少 判断不为空 或不等于0赋值0 ，否则 值+ value（点击的是1 或 -1）
+            const resEle = exceptionList.value[resIndex]  //要去的位置定义值
             if (resEle) {
-                // console.log(resEle.clientHeight)
+                // console.log(resEle.clientHeight) //看当前位置像素
                 resEle.scrollIntoView({behavior: "smooth",block:"center"})
-                // refs.messageBox.scrollTop = resEle.offsetTop - resEle.clientHeight*2
+                // refs.messageBox.scrollTop = resEle.offsetTop - resEle.clientHeight*2   //按照像素定位
                 currentException.value = resIndex
+                // console.log("==data.currentException=",data.currentException)
+                //判断上下按钮是否可点
+                if(currentException.value +1 >= exceptionList.value.length){
+                    up_scrollTo_down.value = true
+                }else if(currentException.value <1){
+                    up_scrollTo.value = true
+                }else{
+                    up_scrollTo.value=false
+                    up_scrollTo_down.value=false
+                }
             }
+        }
+        const handleBlur=(sele)=>{
+            console.log("jinlai")
+            exceptionList.value = refs.exception
+            data.options = exceptionList.value
+              if (sele) {
+                const resEle = exceptionList.value[sele]
+                resEle.scrollIntoView()
+                currentException.value = sele  //当前位置
+                if(currentException.value +1 >= exceptionList.value.length){
+                    up_scrollTo_down.value = true
+                }else if(currentException.value <1){
+                    up_scrollTo.value = true
+                }else{
+                    up_scrollTo.value=false
+                    up_scrollTo_down.value=false
+                }
+            }
+
         }
       return {
         dialogVisible,
-          data,close,openDialog,handleDialogClose,log_flush,scrollTo,
-          loading,quanping,All_Quanping,Quanping_Rest,Cat_Trace,dialog_show_detail,infoPod,guanjianzi
+          data,close,openDialog,handleDialogClose,log_flush,scrollTo,up_scrollTo,up_scrollTo_down,handleBlur,exceptionList,currentException,
+          loading,quanping,All_Quanping,Quanping_Rest,Cat_Trace,dialog_show_detail,guanjianzi
       }
     }
     }
@@ -171,9 +222,7 @@
         color: red;
         background-color: yellow;
     }
-   .el-dialog_title {
-    line-height: 24px;
-    font-size: 18px;
-    color: red;
-    }
 </style>
+<style>
+    .el-dialog__headerbtn{position:absolute;top:5px;right:20px;padding:0;background:0 0;border:none;outline:0;cursor:pointer;font-size:30px}
+    </style>
